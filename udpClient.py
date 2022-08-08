350bf503e6531a9334e48ff17954a8d6bb2fb75a
ayonchev <24881674+ayonchev@users.noreply.github.com>
2022-08-08 13:27:26 +0200

    Refactoring.

--- udpClient.py
+++ udpClient.py
@@ -32,10 +32,6 @@ server_port = 12345			        # specified port to connect
 
 paths_count = 5
 
-
-client_port = 12000
-sock.bind(("127.0.0.1", client_port))
-
 print("UDP target IP:", server_ip)
 print("UDP target Port:", server_port)
 
@@ -84,64 +80,4 @@ else:
                 sequence_number += 1
             break
         else:
-           # input_video.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, pos_frame-1)
-
-            '''
-        if input_video.get(cv2.cv.CV_CAP_PROP_POS_FRAMES) == input_video.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT):
-            size = 10
-            p = struct.pack("I", size)
-            sock.send(p)
-            sock.send('')
-            break
-            '''
-
-#for m in msg:
-    #sock.sendto(str(m).encode(),(udp_host,udp_port))		# Sending message to UDP server
- #   pass
-
-
-
-'''
-
-sport = 4711    # arbitrary source port
-dport = 45134   # arbitrary destination port
-length = 8 + len(msg)
-checksum = 0
-
-start = time.time()
-
-src_addr = socket.handshake.address
-dest_addr
-
-##First way
-zero = 0
-protocol = socket.IPPROTO_UDP 
-data_len = len(msg)
-udp_length = 8 + data_len
-src_ip, dest_ip = ip2int(src_addr[0]), ip2int(dest_addr[0])
-src_ip = struct.pack('!4B', *src_ip)
-#Get source ip for obtaining udp header
-
-pseudo_header = struct.pack('!BBH', zero, protocol, udp_length)
-pseudo_header = src_ip + dest_ip + pseudo_header
-udp_header = struct.pack('!4H', src_port, dest_port, udp_length, checksum)
-checksum = checksum_func(pseudo_header + udp_header + msg)
-udp_header = struct.pack('!4H', src_port, dest_port, udp_length, checksum)
-
-udp_header = struct.pack(str(start), sport, dport, length, checksum)
-
-sock.sendto(bytes(udp_header + msg, encoding='utf-8'),(udp_host,udp_port))		# Sending message to UDP server
-
-'''
-
-#sock.sendto(bytes(msg, encoding='utf-8'),(udp_host,udp_port))		# Sending message to UDP server
-
-
-#Our udpServer.py is up and running, so now we try to run the udpclient.py script,
-
-
-
-def ip2int(ip_addr):
-    if ip_addr == 'localhost':
-        ip_addr = '127.0.0.1'
-    return [int(x) for x in ip_addr.split('.')]
+           pass
--- udpServer.py
+++ udpServer.py
@@ -1,45 +1,27 @@
 #!usr/bin/python
 
+from operator import contains
 import socket
 import struct
-import threading
 import time
 from datetime import datetime
 import numpy as np
-
-## Ressources based on:
-## https://tutorialmeta.com/question/python-udp-socket-has-packet-loss-every-65536-packets
-
-def receive():
-	global data
-	global addr
-	while not kill.is_set():
-		d, addr = sock.recvfrom(512)
-		data.append(d)
-		address.append(addr)
 
 sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)      # For UDP
 
 udp_host =  "127.0.0.1" #socket.gethostbyname("")	        # Host IP
 udp_port = 12345			                # specified port to connect
 
-
-#print type(sock) ============> 'type' can be used to see type 
-				# of any variable ('sock' here)
-
-#sock.bind((udp_host,udp_port))
-miss = 0
-#kill = threading.Event()
-#p = threading.Thread(target = receive())
-#p.start()
 databus = []
 count = 0
 total = 0
-total_miss = 0
 
 t1 = []
 t2 = []
 Dj = []
+
+expected_sequence_number = 1
+missing_packets = []
 
 while True:
 	try:
@@ -60,6 +42,16 @@ try:
 		data = packet[12:]	#get data from packet
 
 		path_id, start_time, sequence_number = struct.unpack('bii', headers)	#get path_id and time_stamp from headers
+
+		print("expected seq num, seq num", (expected_sequence_number, sequence_number))
+		if(expected_sequence_number != sequence_number):
+			if(sequence_number in missing_packets):
+				missing_packets.remove(sequence_number)
+			else:
+				missing_packets.append(expected_sequence_number)
+
+		expected_sequence_number += 1
+
 		end_time = int(time.time())
 
 		delay = end_time - start_time
@@ -75,35 +67,12 @@ try:
 		jitter = j.mean() #Jitter is meand deviation of delays 
 		
 		count+=1
-		#kill.set()
-		#time.sleep(1)
 		size = len(data)	# get size of data
 		databus.append(packet)
 		total = total + size
-		miss = 0
 		length = len(databus)
-		step = 0
 
-		start = int.from_bytes(databus[0][0:4], byteorder='big')
-		end = int.from_bytes(databus[length-1][0:4], byteorder='big')
-		print("start: ",start)
-		print("end: ",end)
-		total = end - start + 1
-		print("Number of packets: ",total)
 		
-		if len(databus) >= 1000:
-			for i in range(0, size-1):
-				current = int.from_bytes(databus[i][0:4], byteorder='big')
-				next = int.from_bytes(databus[i+1][0:4], byteorder='big')
-				step = next - current
-				if step < 0:
-					print("out of order detected!!!")
-					break
-
-			miss = miss + step
-		
-		total_miss = total_miss + miss
-		#"""
 		print("Received Message:",data," from ",addr)
 
 		print("Start Time: ", start_time)
@@ -115,57 +84,11 @@ try:
 
 		print("Jitter: ", jitter)
 
-		print("Step: ", step)
 		print("Size of Message: "+str(size))
 		print("Count Messages sent: " +  str(count))
 
-		print("Number of missing packets: ",miss)
-		print("Number of packets: ",total)
-		print("Total Miss: ", total_miss)
-		print("Packet loss: ", float(miss/size))
-		print("Packet loss Total: ", float(total_miss/total))
-		#"""
+		print("Missing packets: ", missing_packets)
+		print("Number of missing packets: ", len(missing_packets))
+		print("Total bytes received: ",total)
 except KeyboardInterrupt:
     pass
-	
-
-	#sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-	#sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	#sock.bind(('', addr))
-
-	#sock1 = sock.setsockopt(socket.IPPROTO_IPV6, socket.IP_HDRINCL, 1)
-	#print(sock1)
-
-'''
-	rtt = []
-
-while True:
-	print("Waiting for client...")
-	data,addr = sock.recvfrom(1024)	        #receive data from client
-
-	start=time.time() #the current time
-	while sequence_number<=10:
-		data = ('PING %d %d' % (sequence_number, start)) #client message
-		try:
-			data, addr = sock.recvfrom(1024) #recieves message from server
-			elapsed = (time.time()-start) # calculates the rtt
-			rtt.append(elapsed)
-			print(data)
-			print('Round Trip Time is:' + str(elapsed) + ' seconds')
-		except socket.timeout: #if no reply within 1 second
-			print(data)
-			print('Request timed out')
-			sequence_number+=1 #incremented by 1
-	print("Received Messages:",data," from",addr)
-
-	print("Received %s from %s:%u" % (data.decode('utf8'), addr[0], addr[1]))
-
-	print('Packet loss rate is:' + str((10-len(rtt))*10)+ ' percent')
-
-	sock = socket.socket(socket.AF_INET6, socket.SOCK_RAW)
-	sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	sock.bind(('', addr[1]))
-
-	#sock1 = sock.setsockopt(socket.IPPROTO_IPV6, socket.IP_HDRINCL, 1)
-	#print(sock1)
-'''
